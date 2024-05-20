029498845777d0031b6517677ed8b2fb985e9972
zelyankovas <vas.zelenko@gmail.com>
2024-05-20 20:48:33 +0300

    change 01 sprint

--- 1-todolist/1-todolist/src/App.tsx
+++ 1-todolist/1-todolist/src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
+
+// Create
+// Read (view mode, filter, sort, search, page(по 10, 20 страниц показывать)
+// Update
+// Delete
+// CRUD операции -> данные непрерывно меняются
+
+// CLI -> командная строка
+// GUI -> Graphic user interface -> мы такое делаем
+// VUI -> voice
+// VRUI -> виртуальная реальность
+// ARUI -> дополненная реальность
+
 
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title="What to learn"/>
+            {/* Todolist({title: "What to learn"})*/}
+            <Todolist title="What to buy"/>
+            <Todolist title="What to read"/>
         </div>
     );
 }
